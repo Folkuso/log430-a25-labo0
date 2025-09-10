10953264165d9f43f852cc85448c3f6e4bc091e9
Jonathan Savard <jonathan.savard.3@ens.etsmtl.ca>
2025-09-10 09:53:33 -0400

    Test pour calcultor.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -8,6 +8,24 @@ from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
+
+def test_subtraction():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(2,3) == -1
+
+def test_multiplication():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(2,4) == 8
+
+def test_division():
+    my_calculator = Calculator()
+    assert my_calculator.division(6,2) == 3
+
+def test_division_par_zero():
+    my_calculator = Calculator()
+    assert my_calculator.division(1000,0) == "Erreur : division par zéro"
